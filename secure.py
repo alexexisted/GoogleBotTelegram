c02c9d1e959ba4580682951e3013ba67fc9781d5
alexexisted <a.assjones@list.ru>
2023-11-29 16:01:18 +0300

    added gitignore

--- secure.py
+++
@@ -1,6 +0,0 @@
-BOT_TOKEN = "6428549800:AAGC4VYwauwge6pbO-WXJ7EyIINw5Uj3u-E"
-
-client_secret_calendar = '/Users/alexag/Desktop/googleBot/creds1.json'
-
-calendarId = '3849e27ee5d66228f160fe12d6ba15dc5399e96d573787fb5190d45884c57b64@group.calendar.google.com'
-
